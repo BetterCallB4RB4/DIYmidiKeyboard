cee06992189c3c80b3c588ca72552671f16cbf67
Augusto Barbadoro <augusto.barbadoro244@gmail.com>
2022-07-03 13:51:08 +0200

    add callback

--- Mellowtron/Mellowtron.ino
+++ Mellowtron/Mellowtron.ino
@@ -2,7 +2,6 @@
 #include "TimerOne.h"
 
 #define PIN_CONTROL 9
-
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 /*
@@ -11,106 +10,30 @@ MIDI_CREATE_DEFAULT_INSTANCE();
  * (since the pitch is controlled by the quality of the motor) 
  * the notes must be mapped manually
  */
+//int pitchMap[ 12 ][ 2 ] = {{0, 200}, {1, 250}, {2, 300}, {3, 350}, {4, 400}, {5, 450}, {6, 500}, {7, 550}, {8, 600}, {9, 650}, {10, 700}, {11, 750}};
+int pitchMap[ 12 ][ 2 ] = {{0, 20}, {1, 40}, {2, 60}, {3, 80}, {4, 100}, {5, 120}, {6, 150}, {7, 170}, {8, 190}, {9, 210}, {10, 230}, {11, 250}};
 
-/*
- * We have estimated to reach 3 octaves, to move further in range 
- * it will be necessary to modify the pitch present on the cassette. 
- * 
- * a C must be recorded on the cassette
- */
-int pitchMap[ 12 ][ 2 ] = {{61, 200}, {62, 250}, {63, 300}, {64, 350}, {65, 400}, {66, 450}, {67, 500}, {68, 550}, {69, 600}, {70, 650}, {71, 700}, {72, 750}};
+void setup() { 
+  /* var for midi*/
+  Serial.begin(31250);
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  MIDI.setHandleNoteOn(handleNoteOn);
 
-
-void setup() {  
-  MIDI.begin();
-  pinMode(LED_BUILTIN, OUTPUT);
+  /* var for feedback live led */
+  pinMode (LED_BUILTIN, OUTPUT); 
   digitalWrite(LED_BUILTIN, LOW);
+
+  /* var for pwm pitch control */
   
   pinMode(PIN_CONTROL, OUTPUT);
+  /*
   Timer1.initialize(1);
-  Timer1.pwm(PIN_CONTROL, 512, 1); //first initialization - allows to use setPwmDuty() later wich is slitly faster function
+  Timer1.pwm(PIN_CONTROL, 512, 1);  //first initialization - allows to use setPwmDuty() later wich is slitly faster function 
+  */
 }
 
-String inString = "";
-int value = 0;
-int previus = 0;
-
 void loop() {
-
-  if (Serial.available()){
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-  }
-
-  
-
-  
-  
-  /*
-  if (MIDI.read()){
-    switch(MIDI.getType()){
-      case midi::NoteOn:
-        int note = int(MIDI.getData1());
-        playNote(note);
-        break;
-    }
-  }
-  */
-
-
-
-  /*
-  //inside loop()
-  // Read serial input:
-  while (Serial.available() > 0) {
-    int inChar = Serial.read();
-    if (isDigit(inChar)) {
-      // convert the incoming byte to a char and add it to the string:
-      inString += (char)inChar;
-    }
-    // if you get a newline, print the string, then the string's value:
-    if (inChar == '\n') {
-      value = inString.toInt();
-      Serial.println(value);
-      inString = "";
-    }
-  }
-  if(previus != value){
-    Timer1.setPwmDuty(PIN_CONTROL, value);
-    previus = value;
-  }
-  */
-   
-   /*
-   delay(tempo);
-   playNote(62);
-   delay(tempo);
-   playNote(63);
-   delay(tempo);
-   playNote(64);
-   delay(tempo);
-   playNote(65);
-   delay(tempo);
-   playNote(66);
-   delay(tempo);
-   playNote(67);
-   delay(tempo);
-   playNote(68);
-   delay(tempo);
-   playNote(69);
-   delay(tempo);
-   playNote(70);
-   delay(tempo);
-   playNote(71);
-   delay(tempo);
-   playNote(72);
-   delay(tempo);
-   */
-   
-  
-   
+   MIDI.read();
 }
 
 /*
@@ -119,11 +42,14 @@ void loop() {
  * (which is delegated to the minilogue circuit) so the note is never stopped. 
  * The audio stream is continuous, it only change its pitch over time
  */
-void playNote(int midiNote){
+void handleNoteOn(byte channel, byte pitch, byte velocity){
+  /* pitch control */
+  int midiNote = int(pitch)%12;
   for(int i = 0; i < 12; i++){
     if(midiNote == pitchMap[i][0]){
-      Timer1.setPwmDuty(PIN_CONTROL, pitchMap[i][1]);
-      Serial.println(pitchMap[i][1]);
+      //Timer1.setPwmDuty(PIN_CONTROL, pitchMap[i][1]);
+      //Timer1.pwm(PIN_CONTROL, 512, 1); less optimized as the doc tells
+      analogWrite(PIN_CONTROL, pitchMap[i][1]);
       return;
     }
   }
